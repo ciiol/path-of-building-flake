054ae5a719f1bf64d856f7e833b9dd66cf3d9a49
Andrey Fadeev <me@ciiol.net>
2023-04-30 19:11:52 +0200

    Update PoB to 2.29.0

--- path-of-building.nix
+++ path-of-building.nix
@@ -2,12 +2,12 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "path-of-building";
-  version = "2.28.0";
+  version = "2.29.0";
   name = "path-of-building-${version}";
 
   src = fetchTarball {
     url = "https://github.com/PathOfBuildingCommunity/PathOfBuilding/archive/refs/tags/v${version}.tar.gz";
-    sha256 = "14y2h2mp9ppifaxikb3a6vff424pa31cvkaqdri36wlf9r8amvi0";
+    sha256 = "0xa1lw95v2cpgh40yl7lxymsklka4xldkw5ffxr6x81ykmpr0vxq";
   };
 
   nativeBuildInputs = [
