792216a749238ea538a085fc01a039acea4ed8aa
Andrey Fadeev <me@ciiol.net>
2023-08-25 00:04:05 +0200

    Show the correct PoB version
    This commit creates a fake manifest because PoB treats the one from
    the repository as something for "development" only. Luckily, we
    don't need a working auto-updating, so the manifest may be very simple.

--- path-of-building.nix
+++ path-of-building.nix
@@ -22,6 +22,16 @@ pkgs.stdenv.mkDerivation rec {
     cp -r spec/ $out/spec
     cp -r src/ $out/src
 
+    cp changelog.txt help.txt $out/src
+    touch $out/installed.cfg
+
+    cat >$out/manifest.xml <<EOL
+    <?xml version='1.0' encoding='UTF-8'?>
+    <PoBVersion>
+      <Version number="${version}" branch="release" platform="nix"/>
+    </PoBVersion>
+    EOL
+
     cat >$env <<EOL
     export LUA_PATH='$out/runtime/lua/?.lua;$out/runtime/lua/?/init.lua'
     export LUA_CPATH='${luaEnv}/lib/lua/${luaEnv.lua.luaversion}/?.so'
